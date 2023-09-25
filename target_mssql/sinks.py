af0941f100b249b0c21b5da886c29e11423264a5
Dan Norman <buzzcutnorman@gmail.com>
2023-09-25 13:21:09 -0700

    add allow_overwrite to mssqlConnector

--- target_mssql/sinks.py
+++ target_mssql/sinks.py
@@ -26,6 +26,7 @@ class mssqlConnector(SQLConnector):
     allow_column_rename: bool = True  # Whether RENAME COLUMN is supported.
     allow_column_alter: bool = False  # Whether altering column types is supported.
     allow_merge_upsert: bool = False  # Whether MERGE UPSERT is supported.
+    allow_overwrite: bool = False  # Whether overwrite load method is supported.
     allow_temp_tables: bool = True  # Whether temp tables are supported.
 
     def __init__(
